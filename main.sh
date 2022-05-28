6fbceaea74d8aec92d13c02a0afc024020e27124
Matheus <matheusn06leite@gmail.com>
2022-05-28 17:19:16 -0300

    Adicionando função que mostra uma tabela com as médias

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ MENUOPCAO=0
 MENU2=x
 
 menu () {
-    clear
+    
     echo "          =======         "
     echo "          = CAU =         "
     echo "          =======         "
@@ -11,6 +11,9 @@ menu () {
     echo "Selecione uma opcao: "
     echo "1 - Horarios"
     echo "2 - Provas"
+    echo "3 - Anotações"
+    echo "4 - Data"
+    echo "5 - Médias"
     echo "S - SAIR"
 
     read -p "Opcao: " MENUOPCAO
@@ -23,7 +26,7 @@ horarios () {
     echo " SEGUNDA       TERÇA      QUARTA       QUINTA      SEXTA"
     echo "==========================================================="
     echo "RELIGIAO      INGLES      INGLES      PORTUGUES   REDACAO"
-    echo "MATEMATICA    PORTGUES    CAPELA      ESPANHOL    BIOLOGIA"
+    echo "MATEMATICA    PORTUGUES   CAPELA      ESPANHOL    BIOLOGIA"
     echo "BIOLOGIA      HISTORIA    GEOGRAFIA   HISTORIA    ED.FISICA"
     echo "SOCIOLOGIA    LITERATURA  GEOGRAFIA   RELIGIAO    ARTES"
     echo "FISICA        QUIMICA     QUIMICA     PORTUGUES   FILOSOFIA"
@@ -42,12 +45,73 @@ calendarioProvas () {
     echo " INGLES      LITERATURA   REDACAO      PORTUGUES  FILOSOFIA"
 }
 
+media () {
+    clear
+    
+    #Variaveis Materias
+    MATEMATICA=0
+    RELIGIAO=0
+    BIOLOGIA=0
+    SOCIOLOGIA=0
+    FISICA=0
+    INGLES=0
+    PORTUGUES=0
+    HISTORIA=0
+    LITERATURA=0
+    QUIMICA=0
+    REDACAO=0
+    GEOGRAFIA=0
+    EDFISICA=0
+    ESPANHOL=0
+    ARTES=0
+    FILOSOFIA=0
+
+
+    echo "1 - Mostrar médias salvas"
+    echo "2 - Calcular novas médias"
+    read -p "Opcao: " MENU3
+
+    if [ $MENU3 = '1' ]
+    then
+        clear
+        echo "                    MEDIAS                "
+        echo "============================================="
+        echo "|                     |                     |"
+        echo "|     Matematica: $MATEMATICA   |   Religiao:   $RELIGIAO     |"
+        echo "|     Biologia:   $BIOLOGIA   |   Sociologia: $SOCIOLOGIA     |"
+        echo "|     Fisica:     $FISICA   |   Ingles:     $INGLES     |"
+        echo "|     Portugues:  $PORTUGUES   |   Historia:   $HISTORIA     |"
+        echo "|     Literatura: $LITERATURA   |   Quimica:    $QUIMICA     |"
+        echo "|     Redacao:    $REDACAO   |   Geografia:  $GEOGRAFIA     |"
+        echo "|     Ed. Fisica: $EDFISICA   |   Espanhol:   $ESPANHOL     |"
+        echo "|     Artes:      $ARTES   |   Filosofia:  $FILOSOFIA     |"
+        echo "============================================="
+        
+
+    fi
+
+}
+
+#anotacoes () {
+        #echo $(vim anotacoes.txt)
+#}
+
+#tarefa () {
+#        read -p "Quantas tarefas deseja criar?" QTDTAREFAS
+#
+#       for sequencia in $(seq 1 $QTDTAREFAS)
+#        do
+#            
+#        done
+#}
+
 
 
 
 
 while [ $MENUOPCAO != "s" ]
 do
+    clear
     menu
     case $MENUOPCAO in
         1) 
@@ -69,6 +133,30 @@ do
             then
                 continue
             fi
+        ;;
+
+#        3) 
+#            anotacoes
+#        ;;
+
+        4) 
+            clear
+            echo "$(date)"
+            sleep 2
+        ;;
+
+        5) 
+            media
+            read -p "(sair/s): " $MENU2
+            if [ $MENU2 = 's' || $MENU2 = 'sair' ]
+            then
+                continue
+            fi
+        ;;
+
+        s) 
+            echo "Saindo..."
+            sleep 1
         ;;
 
         *) 
